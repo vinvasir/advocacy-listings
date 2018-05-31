0ecc6fa5df560a43e0ef1e5c70e4166eddea1ffe
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-05-31 19:32:51 -0400

    successfully set the area in the session

--- app/controllers/session/areas_controller.rb
+++ app/controllers/session/areas_controller.rb
@@ -5,7 +5,7 @@ module Session
 
       session[:area_id] = @area.id if @area
 
-      redirect_to area_path(@area)
+      redirect_to categories_path(@area)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '/session/areas/:id', to: 'session/areas#create'
+  get '/session/areas/:id', to: 'session/areas#create', as: 'area'
 
-  get '/areas/:id', to: 'categories#index', as: 'area' do
+  get '/areas/:id', to: 'categories#index', as: 'categories' do
 
     get '/listings/:id', to: 'listings#show'
   end
