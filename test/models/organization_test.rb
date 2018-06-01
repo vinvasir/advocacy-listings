9eaac9a59e667084c775c405566dc4f47fc70a36
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-01 09:47:44 -0400

    also test the from_category scope

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -4,19 +4,36 @@ class OrganizationTest < ActiveSupport::TestCase
   def setup
     @usa = Area.create!(name: "United States")
     @south = @usa.children.create!(name: "US South")
-    @atlanta = @south.children.create!(name: "Atlanta")
-    @nashville = @south.children.create!(name: "Nashville")
 
-    cat = Category.create!(name: "some category")
 
-    @fake_atlanta = @atlanta.organizations.create!(title: "Fake Atlanta Org", mission: "idk", category: cat)
-    @fake_nashville = @nashville.organizations.create!(title: "Fake Nashville", mission: "idk", category: cat)
+    @cat = Category.create!(name: "some category")
   end
 
   test "scope in_area should return all orgs in an area or its sub-areas" do
+    @atlanta = @south.children.create!(name: "Atlanta")
+    @nashville = @south.children.create!(name: "Nashville")
+
+    @fake_atlanta = @atlanta.organizations.create!(title: "Fake Atlanta Org", mission: "idk", category: @cat)
+    @fake_nashville = @nashville.organizations.create!(title: "Fake Nashville", mission: "idk", category: @cat)
+
     orgs = Organization.in_area(@south)
 
     assert_includes orgs, @fake_nashville
     assert_includes orgs, @fake_atlanta
+    assert_includes Organization.in_area(@usa), @fake_atlanta
+    assert_includes Organization.in_area(@usa), @fake_nashville
+  end
+
+  test "scope from_category should return all orgs in a category or its sub-categories" do
+    @transit = @cat.children.create!(name: "Transit")
+    @cycling = @transit.children.create!(name: "Cycling")
+
+    other_cat = Category.create!(name: "Irrelevant")
+
+    fake_org = @cycling.organizations.create!(title: "Fake Org", area: @usa, mission: "wevs")
+
+    assert_includes Organization.from_category(@cat), fake_org
+    assert_includes Organization.from_category(@cycling), fake_org
+    refute_includes Organization.from_category(other_cat), fake_org
   end
 end
