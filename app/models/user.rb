e35c370b95b69a42772f83d7cad67b042bff4936
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-01 13:00:47 -0400

    add config for gdpr_rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def is_admin?
+    email == "prat.vasireddy@gmail.com"
+  end
 end
---
+++ config/initializers/gdpr_rails.rb
@@ -0,0 +1,14 @@
+config = PolicyManager::Config.setup do |c|
+  c.logout_url = "/users/sign_out"
+  c.from_email = "admin@advocacy-db.herokuapp.com"
+  c.admin_email_inbox = "prat.vasireddy@gmail.com"
+
+  c.user_resource = User # defaults to User
+  c.admin_user_resource = User # defaults to User
+  # is_admin method in order for engine to know
+  # how to authorize admin only areas
+  c.is_admin_method = ->(o){
+    o.is_admin?
+  }
+
+end
