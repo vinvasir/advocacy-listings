64ce405007318863bd52bccc85c6db87465068cd
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-13 11:20:43 -0400

    also use the factory in orgs test

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -2,16 +2,15 @@ require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
   def setup
-    @usa = Area.create!(name: "United States")
-    @south = @usa.children.create!(name: "US South")
-
+    @usa = FactoryBot.create(:root_area)
+    @south = Area.find_by(name: "US South")
 
     @cat = Category.create!(name: "some category")
   end
 
   test "scope in_area should return all orgs in an area or its sub-areas" do
-    @atlanta = @south.children.create!(name: "Atlanta")
-    @nashville = @south.children.create!(name: "Nashville")
+    @atlanta = Area.find_by(name: "Atlanta")
+    @nashville = Area.find_by(name: "Nashville")
 
     @fake_atlanta = @atlanta.organizations.create!(title: "Fake Atlanta Org", mission: "idk", category: @cat)
     @fake_nashville = @nashville.organizations.create!(title: "Fake Nashville", mission: "idk", category: @cat)
