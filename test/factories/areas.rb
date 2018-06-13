39dc009c6a30f34d2669183b02abadc09c4bc567
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-13 11:15:35 -0400

    update area test to use the factory

--- test/factories/areas.rb
+++ test/factories/areas.rb
@@ -2,49 +2,43 @@ FactoryBot.define do
   factory :root_area, class: Area do
     initialize_with { new(name: "United States") }
 
-    after(:save) do |usa|
+    after(:create) do |usa|
       regions = []
 
-      regions << usa.children.new(name: "East Coast")
-      regions << usa.children.new(name: "Midwest")
-      regions << usa.children.new(name: "US South")
-      regions << usa.children.new(name: "West Coast")
+      regions << usa.children.create!(name: "East Coast")
+      regions << usa.children.create!(name: "Midwest")
+      regions << usa.children.create!(name: "US South")
+      regions << usa.children.create!(name: "West Coast")
 
-      regions.each &:save
+      regions[0].children.create!(name: "NewYork City")
+      regions[0].children.create!(name: "Boston")
+      regions[0].children.create!(name: "Philadelphia")
 
-      regions[0].children.new(name: "New York City")
-      regions[0].children.new(name: "Boston")
-      regions[0].children.new(name: "Philadelphia")
+      regions[1].children.create!(name: "Chicagoland")
+      regions[1].children.create!(name: "Detroit")
+      regions[1].children.create!(name: "Indiana")
+      regions[1].children.create!(name: "Minnesota")
+      regions[1].children.create!(name: "Ohio")
+      regions[1].children.create!(name: "Wisconsin")
 
-      regions[1].children.new(name: "Chicagoland")
-      regions[1].children.new(name: "Detroit")
-      regions[1].children.new(name: "Indiana")
-      regions[1].children.new(name: "Minnesota")
-      regions[1].children.new(name: "Ohio")
-      regions[1].children.new(name: "Wisconsin")
+      regions[2].children.create!(name: "Alabama")
+      regions[2].children.create!(name: "Atlanta")
+      regions[2].children.create!(name: "Charlotte")
+      regions[2].children.create!(name: "Louisiana")
+      regions[2].children.create!(name: "Mississippi")
+      regions[2].children.create!(name: "Nashville")
+      regions[2].children.create!(name: "NC Triangle")
+      regions[2].children.create!(name: "New Orleans")
+      regions[2].children.create!(name: "Richmond")
 
-      regions[2].children.new(name: "Alabama")
-      regions[2].children.new(name: "Atlanta")
-      regions[2].children.new(name: "Charlotte")
-      regions[2].children.new(name: "Louisiana")
-      regions[2].children.new(name: "Mississippi")
-      regions[2].children.new(name: "Nashville")
-      regions[2].children.new(name: "NC Triangle")
-      regions[2].children.new(name: "New Orleans")
-      regions[2].children.new(name: "Richmond")
-
-      regions[3].children.new(name: "Berkeley")
-      regions[3].children.new(name: "Los Angeles")
-      regions[3].children.new(name: "Oakland")
-      regions[3].children.new(name: "Portland")
-      regions[3].children.new(name: "San Diego")
-      regions[3].children.new(name: "San Francisco")
-      regions[3].children.new(name: "Seattle")
-      regions[3].children.new(name: "Silicon Valley")
-
-      regions.each do |region|
-        region.children.each &:save
-      end
+      regions[3].children.create!(name: "Berkeley")
+      regions[3].children.create!(name: "Los Angeles")
+      regions[3].children.create!(name: "Oakland")
+      regions[3].children.create!(name: "Portland")
+      regions[3].children.create!(name: "San Diego")
+      regions[3].children.create!(name: "San Francisco")
+      regions[3].children.create!(name: "Seattle")
+      regions[3].children.create!(name: "Silicon Valley")
     end
   end
 end
--- test/models/area_test.rb
+++ test/models/area_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class AreaTest < ActiveSupport::TestCase
   def setup
-    @usa = FactoryBot.create(:root_area) # Area.create!(name: "United States")
-    @south = @usa.children.create!(name: "US South")
-    @atlanta = @south.children.new(name: "Atlanta")
-    @nashville = @south.children.new(name: "Nashville")
+    @usa = FactoryBot.create(:root_area)
+    @south = Area.find_by(name: "US South")
+    @atlanta = Area.find_by(name: "Atlanta")
+    @nashville = Area.find_by(name: "Nashville")
   end
 
   test "should set a prefixed slug for grandchildren and below of the parent node" do
     assert_equal "united-states", @usa.slug
-    # assert_equal "us-south", @south.slug
-    # assert_equal "us-south-atlanta", @atlanta.slug
-    # assert_equal "us-south-nashville", @nashville.slug
+    assert_equal "us-south", @south.slug
+    assert_equal "us-south-atlanta", @atlanta.slug
+    assert_equal "us-south-nashville", @nashville.slug
   end
 end
