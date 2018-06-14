3ff1af70784e21b684212af3195a45c0030bb50c
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-14 19:21:08 -0400

    set up algolia indexing attributes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ApplicationRecord
+  include AlgoliaSearch
+
   belongs_to :area
   belongs_to :category
   belongs_to :submitter, class_name: "User", foreign_key: "user_id", optional: true
@@ -9,9 +11,45 @@ class Organization < ApplicationRecord
   has_many :claims
   has_many :claimants, through: :claims, class_name: "User", source: :user
 
+  # has_many :approved_claims, -> { where approved: true }, class_name: "Claim"
+
   scope :in_area, -> (area) { where(area_id: area.descendant_ids.push(area.id)) }
   scope :from_category, -> (category) { where(category_id: category.descendant_ids.push(category.id)) }
 
   scope :live, -> { where(live: true) }
   scope :not_live, -> { where.not(live: true) }
+
+  algoliasearch per_environment: true, unless: :hide_from_algolia?, disable_indexing: Rails.env.test? do
+    attribute :id, :title, :mission, :created_at, :updated_at
+
+    attribute :area do
+      { 
+        id: area.id, 
+        name: area.name, 
+        slug: area.slug,
+        parent_id: area.parent_id
+      }
+    end
+
+    attribute :category do
+      {
+        id: category.id,
+        name: category.name,
+        slug: category.slug,
+        parent_id: category.parent_id
+      }
+    end
+
+    # attribute :owners do
+    #   owners.map do |owner|
+    #     {
+    #       email: owner.email
+    #     }
+    #   end
+    # end
+  end
+
+  def hide_from_algolia?
+    self.live?
+  end  
 end
